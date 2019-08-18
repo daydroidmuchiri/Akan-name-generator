44f865facb55f80a37320681ed91975c948a1992
daydroid <daydroidmuchiri@gmail.com>
2019-08-18 11:49:36 +0300

    create variables

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function AkanName() {
+
+    var DD = parseFloat(document.getElementById("dd").value);
+    var MM = parseFloat(document.getElementById("mm").value);
+    var YY = parseFloat(document.getElementById("yy").value);
