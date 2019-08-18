fe5be5894438d3c2045cb1f6023efa79a31ec7bb
daydroid <daydroidmuchiri@gmail.com>
2019-08-18 12:05:58 +0300

    add arrays

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,6 @@ function AkanName() {
     var DD = parseFloat(document.getElementById("dd").value);
     var MM = parseFloat(document.getElementById("mm").value);
     var YY = parseFloat(document.getElementById("yy").value);
+    var CC = (YY - 1) / 100 + 1;
+    var arrayofDays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+    var dayoftheweek = (CC / 4 - 2 * CC - 1 + 5 * YY / 4 + (26 * (MM + 1) / 10) + DD) % 7;
